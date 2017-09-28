17847447c66a484fe9a5aa300c27cff8dd32d4ee
Paul McCormick <paul@effigy.com.au>
2017-09-28 13:21:08 +1300

    Add unique ID to accordian
    Fixes toggle when more than one accordian on a page

--- modules/accordion/accordion.php
+++ modules/accordion/accordion.php
@@ -7,16 +7,20 @@ Description: Display Accordian content
 
 if( have_rows('accordion') ): ?>
 
-    <!-- Panels -->
-	<div class="panel-group" id="accordion">
-
 	<?php // Start count of accordion items;
 	$counter = 0;
+	$randomGroupID = uniqid();
+	?>
 	
-	while( have_rows( 'accordion' ) ): the_row(); 
+    <!-- Panels -->
+	<div class="panel-group" id="accordion-<?php echo $randomGroupID ;?>">
+
+	<?php
+		while( have_rows( 'accordion' ) ): the_row(); 
 		$icon  = get_sub_field('icon');
 		$title = get_sub_field('title');
 		$text  = get_sub_field('text');
+		$randomID = uniqid();
 	?>
 	
 	<div class="panel panel-default">
@@ -33,14 +37,14 @@ if( have_rows('accordion') ): ?>
 		        	 	$collapsed = 'collapsed';
 		        	 } ?>
 		        
-		            <a class="accordion-toggle collapse-header collapse <?php echo $collapsed;?>" data-toggle="collapse" data-parent="#accordion" data-target="#bml-0<?php print( $counter + 1 ); ?>" aria-expanded="<?php echo $ariaExpanded;?>">
+		            <a class="accordion-toggle collapse-header collapse <?php echo $collapsed;?>" data-toggle="collapse" data-parent="#accordion-<?php echo $randomGroupID ;?>" data-target="#efflab-<?php echo $randomID ;?>-0<?php print( $counter + 1 ); ?>" aria-expanded="<?php echo $ariaExpanded;?>">
 		                <i class="fa fa-fw <?php echo $icon; ?>" aria-hidden="true"></i> <?php echo $title ;?>
 		                <i class="fa fa-fw fa-angle-down collapse-icon"></i>
 		            </a>
 		        </h4>
 		    </div>
 		    
-		   <div id="bml-0<?php print( $counter + 1 ); ?>" class="accordion-body collapse <?php echo $collapseIn;?>">
+		   <div id="efflab-<?php echo $randomID ;?>-0<?php print( $counter + 1 ); ?>" class="accordion-body collapse <?php echo $collapseIn;?>">
 		        <div class="panel-body">
 		            
             		<?php if($text){ echo '<p>'.$text.'</p>';} ?>
